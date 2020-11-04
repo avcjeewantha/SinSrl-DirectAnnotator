c6482e61b2e1ed7169f682c4f1dfa2adc3bf56dd
avcjeewantha <avcjeewantha.16@cse.mrt.ac.lk>
2020-11-04 23:21:23 +0530

    displaying the final output

--- finalPredict.py
+++ finalPredict.py
@@ -3,29 +3,23 @@ from model.predIdConfig import PredIdConfig
 from model.srl_model import SRLModel
 
 
-def findInsideTags(i, n, newTags, tags, seqList):
-    if (i == n - 1):
-        return newTags
-    if ('I-' not in tags[i + 1]):
+def findInsideTags(i, n, newTags, tags, seqList): #(i, 4, ['O', 'O', 'B-go.02', 'O'], ['O', 'O', 'B-go.02', 'O'], [])
+    if (i == n - 1 or 'I-' not in tags[i + 1]):
         seqList.append(newTags)
-    else:
+    elif('I-' in tags[i + 1]):
         newTags[i + 1] = tags[i + 1]
-        if (i + 1 == n - 1):
-            return newTags
-        else:
-            findInsideTags(i + 1, n, newTags)
+        findInsideTags(i + 1, n, newTags, tags, seqList)
 
 
-def predWiseTags(tokens, tags, seqList):
+def predWiseTags(tokens, tags, seqList): #(['මම', 'ගෙදර', 'යමි', '.'], ['O', 'O', 'B-go.02', 'O'], [])
     n = len(tokens)
     for i in range(n):
         if ('B-' in tags[i]):
             newTags = ['O' for i in range(n)]
             newTags[i] = tags[i]
-            pred = tags[i].split('-')[-1]
             findInsideTags(i, n, newTags, tags, seqList)
 
-def makeInputToNext(words_raw, seq):
+def makeInputToNext(words_raw, seq): # (['මම', 'ගෙදර', 'යමි', '.'], ['O', 'O', 'B-go.02', 'O'])
     n = len(seq)
     newInput = []
     for i in range(n):
@@ -38,6 +32,12 @@ def makeInputToNext(words_raw, seq):
             newInput.append(sinWord)
     return newInput
 
+def displayOutput(words_raw, seq):
+	for i in range(len(seq)):
+		if('-' in words_raw[i] and '.0' in words_raw[i]):
+			seq[i] = words_raw[i].split('-', 1)[-1]
+	return seq
+
 def main():
     # create instance of config
     predConfig = PredIdConfig()
@@ -45,13 +45,13 @@ def main():
     predModel = SRLModel(predConfig)
     predModel.build()
     predModel.restore_session(predConfig.dir_model)
-    sentence = input("input> ")
-    words_raw = sentence.strip().split(" ")
+    sentence = input("input> ")   #මම ගෙදර යමි .
+    words_raw = sentence.strip().split(" ") #['මම', 'ගෙදර', 'යමි', '.']
     if (words_raw[-1][-1] == '.' and words_raw[-1] != '.'):
         words_raw = words_raw[:-1] + [words_raw[-1][:-1]] + ['.']
     elif (words_raw[-1] != '.'):
         words_raw.append('.')
-    preds = predModel.predict(words_raw)
+    preds = predModel.predict(words_raw)    #['O', 'O', 'B-go.02', 'O']
     predModel.close_session()
     seqList = []
     predWiseTags(words_raw, preds, seqList)
@@ -62,10 +62,11 @@ def main():
     srlModel = SRLModel(srlConfig)
     srlModel.build()
     srlModel.restore_session(srlConfig.dir_model)
-    for seq in seqList:
-        inputToNextModel = makeInputToNext(words_raw, seq)
-        finalPreds = srlModel.predict(inputToNextModel)
-        print(words_raw, finalPreds)
+    for seq in seqList: #[['O', 'O', 'B-go.02', 'O']]
+        inputToNextModel = makeInputToNext(words_raw, seq) #['මම', 'ගෙදර', 'යමි-B-go.02', '.']
+        finalPreds = srlModel.predict(inputToNextModel) #['B-ARG0', 'B-ARG1','pred' , 'O']
+        output = displayOutput(inputToNextModel, finalPreds) #['B-ARG0', 'B-ARG1','B-go.02' , 'O']
+        print(output)
     srlModel.close_session()
 
 if __name__ == "__main__":
